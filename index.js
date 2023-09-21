2de4a1e3608cb82c18afd87454039b73e222d650
Arthurdante <arthurdantebiztrapp@gmail.com>
2023-09-21 19:42:33 -0300

    ambientefeito

---
+++ index.js
@@ -0,0 +1,13 @@
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// const exercicioRoutes = require('./src/routes/exercicio.js');
+// const exercicio2Routes = require('./src/routes/exercicio2.js');
+
+// app.use(express.json())
+// app.use(exercicioRoutes, exercicio2Routes)
+
+// app.listen(port, () => {
+//     console.log(`Servidor rodando na porta ${port}`)
+// })
